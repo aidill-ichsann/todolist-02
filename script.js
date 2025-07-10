1afac9598c13d64b6597e94a41a553d3ed16479c
aidill-ichsann <aidilichsan545@gmail.com>
2025-07-10 22:00:33 +0700

    mengoptimalkan fungsi tombol add

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 const btnAdd = document.querySelector(".btn-add");
 btnAdd.addEventListener("click", () => {
-  const formInput = document.querySelector(".form-input");
-  formInput.style.display = "flex";
+  const formInput = document.querySelector(".form");
+  formInput.classList.toggle("show");
 });
 
 // function bikin list
@@ -17,6 +16,7 @@ const bikinList = (inputList, inputJam) => {
   label.classList.add("checkbox-wrapper");
   const inputcheck = document.createElement("input");
   inputcheck.type = "checkbox";
+  inputcheck.id = "taskcheck";
   const checkcustom = document.createElement("span");
   checkcustom.classList.add("checkbox-custom");
   const spann = document.createElement("span");
@@ -47,7 +47,46 @@ const btnSave = document
     const inputJam = document.querySelector("#inputjam").value;
     console.log(inputList, inputJam);
     bikinList(inputList, inputJam);
-    const formInput = document.querySelector(".form-input");
-    formInput.style.display = "none";
-    const form = document.querySelector('form').reset()
+    const formInput = document.querySelector(".form");
+    formInput.classList.toggle("show");
+    const form = document.querySelector("form").reset();
   });
+
+// bikin garis tengah pada text
+//  const AllcheckBox = document.querySelectorAll()
+//  AllcheckBox.forEach((chechbox)=>{
+//   console.log('done 1');
+
+//   chechbox.addEventListener('change',()=>{
+//     console.log('done 2');
+//     const item = chechbox.closest('.item')
+//     const judul = item.querySelector('.judul')
+//     console.log('done 3');
+
+//     if(chechbox.cheched){
+//       judul.style.textDecoration = 'line-trough'
+//       console.log('done 4');
+//     } else{
+//       judul.style.textDecoration = 'none'
+
+//     }
+//   })
+//  })
+
+const main = document.querySelector("main");
+main.addEventListener("click", (e) => {
+  const checkbox = e.target;
+  if (checkbox.matches('.checkbox-wrapper input[type = "checkbox"]')) {
+    console.log('done 1');
+    const item = checkbox.closest(".item");
+    const judul = item.querySelector(".judul");
+    
+    if (checkbox.checked) {
+      judul.classList.toggle('done')
+      console.log('done 2');
+    } else{
+      judul.classList.toggle('done')
+
+    }
+  }
+});
